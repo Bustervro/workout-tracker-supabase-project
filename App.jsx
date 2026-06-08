1e6a0b93b6de555cf412a14a0fb59a6befb4d5f4
Bustervro <126031898+Bustervro@users.noreply.github.com>
2026-06-08 13:34:37 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <h1>Workout Tracker</h1>
+
+      <h2>Chest Day</h2>
+      <ul>
+        <li>Bench Press - 3 Sets x 10 Reps @ 135 lbs</li>
+        <li>Incline Dumbbell Press - 3 Sets x 8 Reps @ 75 lbs</li>
+      </ul>
+
+      <h2>Leg Day</h2>
+      <ul>
+        <li>Squat - 4 Sets x 8 Reps @ 225 lbs</li>
+        <li>Leg Press - 3 Sets x 12 Reps @ 300 lbs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
